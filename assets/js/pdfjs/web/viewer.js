b2c7d63d28d65d28f9ebad3c9cae27394537ea01
Jules Schoonman <jules.schoonman@gmail.com>
2022-07-28 17:32:59 +0200

    viewer origin update

--- assets/js/pdfjs/web/viewer.js
+++ assets/js/pdfjs/web/viewer.js
@@ -1916,7 +1916,7 @@ const PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 let validateFileURL;
 {
-  const HOSTED_VIEWER_ORIGINS = ["https://paleopsychopop.org", "http://127.0.0.1:4000"];
+  const HOSTED_VIEWER_ORIGINS = ["https://www.paleopsychopop.org"];
 
   validateFileURL = function (file) {
     if (file === undefined) {
